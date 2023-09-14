9e53ef388dbea84711e9b1a33ca4cbafc39a3f82
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-09-14 20:22:27 -0300

    Refactors main class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Entrada de dados
+        Scanner scanner = new Scanner(System.in);
+        int saldoTotal = scanner.nextInt();
+        int valorSaque = scanner.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        //TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
+        if (saldoTotal < valorSaque) {
+            System.out.println("Saldo insuficiente. Saque nao realizado!");
+        } else {
+            int saldoFinal = saldoTotal - valorSaque;
+            System.out.println("Saque realizado com sucesso. Novo saldo: " + saldoFinal);
         }
+        scanner.close();
     }
+
 }
